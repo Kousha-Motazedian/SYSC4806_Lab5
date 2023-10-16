b92a8e9238e2175fb8c4053e3e688ab698b59d25
Kousha-Motazedian <k.d.Motazedian@Gmail.com>
2023-10-16 12:01:33 -0400

    hopefully works now

--- src/test/java/org/example/BuddyInfoTest.java
+++ src/test/java/org/example/BuddyInfoTest.java
@@ -3,10 +3,10 @@ package org.example;
 import org.junit.Test;
 
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
 
 public class BuddyInfoTest {
